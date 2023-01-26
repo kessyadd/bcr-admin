40b2b07ad86385304852a75f3e13f987c4609147
najottama <najottama@gmail.com>
2023-01-26 15:37:05 +0700

    Add Sign In UI

---
+++ src/components/FormSignIn.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { Button, Checkbox, Form, Input } from "antd";
+const onFinish = (values) => {
+  console.log("Success:", values);
+};
+const onFinishFailed = (errorInfo) => {
+  console.log("Failed:", errorInfo);
+};
+const FormSignIn = () => (
+  <>
+    <h1>Welcome, Admin BCR</h1>
+    <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      style={{
+        maxWidth: 600,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: "Please input your username!",
+          },
+        ]}
+      >
+        <Input placeholder="Contoh: johndee@gmail.com" />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: "Please input your password!",
+          },
+        ]}
+      >
+        <Input.Password placeholder="6+ karakter" />
+      </Form.Item>
+
+      <Form.Item
+        name="remember"
+        valuePropName="checked"
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      ></Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Sign In
+        </Button>
+      </Form.Item>
+    </Form>
+  </>
+);
+export default FormSignIn;
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { Col, Row } from "antd";
+import Car from "../img/car_hero.png";
+import FormSignIn from "../components/FormSignIn";
 
-const SignIn = () => {
-  return (
-    <>
-      <h1>Sign In Page</h1>
-    </>
-  );
+const onFinish = (values) => {
+  console.log("Success:", values);
 };
-
-export default SignIn;
+const onFinishFailed = (errorInfo) => {
+  console.log("Failed:", errorInfo);
+};
+const App = () => (
+  <Row>
+    <Col span={6} push={18}>
+      <FormSignIn />
+    </Col>
+    <Col span={18} pull={6}>
+      <img src={Car} alt="Car" />
+    </Col>
+  </Row>
+);
+export default App;
