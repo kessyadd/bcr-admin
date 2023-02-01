3404f54c8ca032e7c0e16bca258e897e2967183a
kessyadd <kessya.025@gmail.com>
2023-02-01 15:34:25 +0700

    create edit and edit car data function, create delete car modal

--- src/pages/EditCar.jsx
+++ src/pages/EditCar.jsx
@@ -1,15 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import APICar from "../apis/APICar";
 import CarForm from "../components/CarForm";
 
 const EditCar = () => {
+  const [car, setCar] = useState();
+  const [carIds, setCarIds] = useState();
+
   useEffect(() => {
     document.title = "BCR Admin - Edit Car";
+    const url = window.location.href;
+    const urlSplit = url.split("/");
+    const id = urlSplit[urlSplit.length - 1];
+
+    const fetchData = async () => {
+      const res = await APICar.getCarById(id);
+      setCar(res.data);
+      setCarIds(id);
+    };
+    // call the function
+    fetchData()
+      // make sure to catch any error
+      .catch(console.error);
   }, []);
-  return (
-    <>
-      <CarForm pageName="editCar" />
-    </>
-  );
+  return <>{car ? <CarForm pageName="editCar" carId={carIds} carData={car} /> : <p>Loading...</p>}</>;
 };
 
 export default EditCar;
