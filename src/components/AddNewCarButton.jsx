2671e2fcad2985bee0610cc2b922dae07f2926ad
kessyadd <kessya.025@gmail.com>
2023-02-15 22:50:45 +0700

    edit paginaton and add new car btn

--- src/components/AddNewCarButton.jsx
+++ src/components/AddNewCarButton.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 import { Button } from "antd";
-import { useNavigate } from "react-router-dom";
 import { PlusOutlined } from "@ant-design/icons";
 import "../assets/css/addNewCarButton.css";
 
 const AddNewCarButton = (carId) => {
-  const navigate = useNavigate();
-  const handleClick = (path) => {
-    navigate(path);
-  };
-
   return (
-    <Button className="btn-add" type="primary" icon={<PlusOutlined />} onClick={() => handleClick("/car-list/add-car")}>
+    <Button className="btn-add" type="primary" icon={<PlusOutlined />} href="/car-list/add-car">
       Add New Car
     </Button>
   );
